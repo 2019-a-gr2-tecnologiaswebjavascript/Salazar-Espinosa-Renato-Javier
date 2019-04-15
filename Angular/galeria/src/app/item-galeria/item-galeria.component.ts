2bec8851273a3b1520bd137383c1f1895546c9cb
RenatoJavi <rjavierse@hotmail.com>
2019-04-14 21:49:04 -0500

    Crear Proyecto  de Angular

---
+++ Angular/galeria/src/app/item-galeria/item-galeria.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-galeria',
+  templateUrl: './item-galeria.component.html',
+  styleUrls: ['./item-galeria.component.css']
+})
+export class ItemGaleriaComponent implements OnInit {
+
+
+  title='licoreria';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
