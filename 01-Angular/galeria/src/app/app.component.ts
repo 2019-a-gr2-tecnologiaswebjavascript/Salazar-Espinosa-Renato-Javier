140795fa6609bb50ed039ef99710ec4a347ab853
RenatoJavi <rjavierse@hotmail.com>
2019-04-26 00:22:43 -0500

    estructura JSON

--- 01-Angular/galeria/src/app/app.component.ts
+++ 01-Angular/galeria/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Floreria';
+
+  arregloDeFlores=[
+new Flor("Papitas","A lo bestia"),
+new Flor("Carnitas","Gorditas"),
+new Flor("chicas","cheveres"),
+  ];
+
+  arregloFloresJS=[
+    {
+      nombre:"Papitas",
+    descripcion:"A lo bestia"
+  },
+  {
+    nombre:"Carnitas",
+    descripcion:"Gorditas"
+
+    },
+    {
+      nombre:"Chicas",
+    descripcion:"cheveres"
+    }
+
+  ]
+
+}
+
+class Flor{
+  constructor(
+    public nombree:string,
+    public descripcionn:string){
+
+    }
 }
--- 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
+++ 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
@@ -19,6 +19,7 @@ export class ItemGaleriaComponent implements OnInit {
   @Input()
   nombreItem;
 
+  url= "https://www.dasmoto.net/fileadmin/slide/2018/dasmoto_bmw_49.jpg";
 
   constructor() { }
 
@@ -33,6 +34,18 @@ export class ItemGaleriaComponent implements OnInit {
     alert('Alertar blur');
   }
 
+
+notas=[1,2,3,4,5,6,7,8,9,10];
+
+  cambiarImagen(){
+  const urlLaguna="https://media-cdn.tripadvisor.com/media/photo-s/0a/af/6f/a8/laguna-de-yahuarcocha.jpg";
+  const urlPerro="https://kiwoko.a.ssl.fastly.net/media/wysiwyg/cabecera-ficha-labrador.jpg";
+  if(this.url===urlLaguna){
+this.url=urlPerro;
+
+  }else{this.url=urlLaguna;}
+  //this.url=url1;
+}
 
 
 
