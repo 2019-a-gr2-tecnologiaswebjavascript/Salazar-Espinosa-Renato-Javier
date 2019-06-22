49d0b5d155c7884de540053d5aecab84127572bf
RenatoJavi <rjavierse@hotmail.com>
2019-06-21 23:43:22 -0500

    Nuevo proyecto

--- 05-ProyectoMongo/src/servicios/https-sails-principal.ts
+++ 05-ProyectoMongo/src/servicios/https-sails-principal.ts
@@ -2,39 +2,43 @@
 
 //2. eXTENDEMOS ESA CLASE
 //USARLA
-
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
 
-export class HttpSailsPrincipal <ClaseEntidad> {
-         protected url: string = 'http://localhost:1337';
+// RESTFUL -> METODOS "="
 
-         protected modelo: string = "";//para los modulos
-            
-constructor(protected  readonly httpClient:HttpClient,
- url:string,
- modelo:string ){
-this.url=url,
-    this.modelo=modelo,
- }
+// CLASE -> Metodos Logica
+// RESTFUL
 
-crear(nuevoRegistro:ClaseEntidad):Observable<ClaseEntidad> {
-        const url = `${this.url}${this.modelo}`;
-        return this.httpClient
-                    .post(url, nuevoRegistro)
-                    .pipe(
-                        map(
-                            (datos)=>{
-                                return datos as ClaseEntidad;
-                        })
-                    );
-    }
+// Extender esa clase
+// Usarla
 
-    
+export class HttpSailsPrincipal<ClaseEntidad> {
+  protected url: string = 'http://localhost:1337';
+  protected modelo: string = '';
 
-         //crear
-         //borrar
-         //actualiazr
-         //buacar por id
-         //buscar todos
-       }
+  constructor(protected readonly httpClient: HttpClient, url: string, modelo: string) {
+    this.url = url;
+    this.modelo = modelo;
+  }
+
+  crear(nuevoRegistro: ClaseEntidad): Observable<ClaseEntidad> {
+    const url = `${this.url}${this.modelo}`;
+    return this.httpClient.post(url, nuevoRegistro).pipe(
+      map(datos => {
+        return datos as ClaseEntidad;
+      })
+    );
+  }
+
+  // Crear
+
+  // Borrar
+
+  // Actualizar
+
+  // BuscarPorId
+
+  // BuscarTodos
+}
